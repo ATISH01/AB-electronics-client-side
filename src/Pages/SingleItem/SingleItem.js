982771c6635f02c63208a08eceececd5ceb39273
ATISH01 <ab.atish.ab@gmail.com>
2022-04-30 11:28:01 +0600

    single item update functionality implemented

--- src/Pages/SingleItem/SingleItem.js
+++ src/Pages/SingleItem/SingleItem.js
@@ -5,12 +5,41 @@ import { useParams } from 'react-router-dom';
 const SingleItem = () => {
     const { Id } = useParams();
     const [single, setSingle] = useState({});
-    const {name,img,description,price,quentity,supplier}=single;
+    const [count,setCount]=useState();
+    const {_id,name,img,description,price,quentity,supplier}=single;
     useEffect(() => {
         fetch(`http://localhost:5000/allItems/${Id}`)
             .then(res => res.json())
             .then(data => setSingle(data))
     }, [])
+    
+    
+    const minus = (ID) =>{
+            const i = single.quentity--;
+            if(i>0)
+            {console.log(`{id:${i}}`);
+            console.log(single);
+        
+            const url = `http://localhost:5000/allItems/${ID}`;
+            fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(single)
+            })
+            .then(res => res.json())
+            .then(data =>{
+                console.log('success', data);
+                 
+                
+                console.log(single.quentity);
+                setSingle({...single, quentity:single.quentity})    
+            })
+            
+        }
+              else return;
+        }
     return (
         <div className='container'>
             <Row md={2} className="g-3 m-5">
@@ -23,7 +52,8 @@ const SingleItem = () => {
                 <p>Price:{price}</p>
                 <p>Quentity:-{quentity}</p>
                 <p>Supplier:-{supplier}</p>
-                <Button>Shipped</Button>
+                <Button onClick={()=>minus(_id)}>Shipped</Button>
+                
             </Col>
         </Row>
         </div>
